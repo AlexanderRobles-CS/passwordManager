0347da08ecbf29d1c717d2deefaf63fd16af05ec
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2023-06-13 20:00:46 -0700

    removed try blocks

--- user.py
+++ user.py
@@ -3,19 +3,13 @@ from encryption import Encryption
 class User:
     def viewPassword(self, encryption):
 
-        try:
-            with open("passwords.txt", "r") as f:                                                         # Read passwords.txt file
-                print("\n")
+        with open("passwords.txt", "r") as f:                                                         # Read passwords.txt file
 
-                for line in f.readlines():
-                    data = line.rstrip()
-                    decryptedData = encryption.decryptPassword(data, encryption.fernetKey)
-                    username, account_name, password = decryptedData.split(" | ")
-                    print("Account:", account_name, ", Username:", username, ", Password:", password)
-
-                print("\n")
-        except:
-            print("No passwords to view.\n")
+            for line in f.readlines():
+                data = line.rstrip()
+                decryptedData = encryption.decryptPassword(data, encryption.fernetKey)
+                username, account_name, password = decryptedData.split(" | ")
+                print("Account:", account_name, ", Username:", username, ", Password:", password)
 
     def addPassword(self, encryption):                                                       # Add new password
         account_name = input("Account name: ")
