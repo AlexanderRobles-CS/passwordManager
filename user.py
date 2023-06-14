950f9b9990cd59f430d1830af94d4aec0bf56c50
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2023-06-13 20:55:54 -0700

    added view specific password functionality

--- user.py
+++ user.py
@@ -1,15 +1,51 @@
+import re
+import os
 from encryption import Encryption
 
 class User:
     def viewPassword(self, encryption):
 
-        with open("passwords.txt", "r") as f:                                                         # Read passwords.txt file
+        while True:
 
-            for line in f.readlines():
-                data = line.rstrip()
-                decryptedData = encryption.decryptPassword(data, encryption.fernetKey)
-                username, account_name, password = decryptedData.split(" | ")
-                print("Account:", account_name, ", Username:", username, ", Password:", password)
+            userInput = input("Do you want to view all passwords or a specific one? (all/one), press q to exit.  ").lower()
+
+            if userInput == "q":
+                break
+
+            if userInput == "all":
+                with open("passwords.txt", "r") as f:                                                         # Read passwords.txt file
+                    
+                    print("\n")
+                    for line in f.readlines():
+                        data = line.rstrip()
+                        decryptedData = encryption.decryptPassword(data, encryption.fernetKey)
+                        username, account_name, password = decryptedData.split(" | ")
+                        print("Account:", account_name, ", Username:", username, ", Password:", password)
+                    
+                    print("\n")
+
+            elif userInput == "one":
+                searchConent = input("Enter the content to search: ")
+                fileName = "passwords.txt"
+                print("\n")
+                if os.path.isfile(fileName):
+                    with open(fileName, 'r') as file:
+                        for line in file:
+                            data = line.rstrip()
+                            decryptedData = encryption.decryptPassword(data, encryption.fernetKey)
+                            
+                            if re.search(searchConent, decryptedData):
+                                username, account_name, password = decryptedData.split(" | ")
+                                print("Account:", account_name, ", Username:", username, ", Password:", password)
+                                print("\n")
+                                break
+                        else:
+                            print(f"Content '{searchConent}' not found in file '{fileName}'")
+                else:
+                    print(f"File '{fileName}' not found")
+            else:
+                print("Invalid mode selected.")
+
 
     def addPassword(self, encryption):                                                       # Add new password
         account_name = input("Account name: ")
